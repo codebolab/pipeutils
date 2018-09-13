05e2ba9351f97ffca82f6b26ddfc3e05cc5250db
elvis ramirez <elvikito@gmail.com>
2018-09-13 12:30:17 -0400

    ls

--- __init__.py
+++ __init__.py
@@ -1,25 +1,42 @@
 import os
-import sys
+import six
+import errno
 
-if sys.version_info.major == 2:
-    import ConfigParser
-else:
+
+if six.PY3:
     import configparser as ConfigParser
+else:
+    import ConfigParser
 
 HOME = os.path.expanduser("~")
+CONFIG_PATH = os.environ.get('PIPE_CONFIG_PATH', '~/.pipeutils/config/')
 
 
-def config(section='default'):
-    paths = [
-        os.environ.get('PIPE_CONFIG_PATH', None),
-        os.path.join(HOME, '.pipe', 'default.conf'),
-    ]
-    path = None
-    if section == 'aws':
+def config(name='base'):
+    '''
+    Args:
+        name (str): The `name` is name of config file.
+    Returns:
+        (dict) a configuration read from a file found in CONFIG_PATH
+    '''
+
+    if name == 'base':
         paths = [
-            os.path.join(HOME, '.pipe', 'aws.conf'),
+            os.environ.get('PIPE_CONFIG_PATH', None),
+            os.path.join(HOME, '.pipeutils', 'config', 'default.conf'),
+            os.path.join(CONFIG_PATH, 'default.conf'),
+        ]
+    elif name == 'aws':
+        print('into')
+        paths = [
+            os.path.join(HOME, '.pipeutils', 'config', 'aws.conf'),
+            os.path.join(CONFIG_PATH, 'aws.conf'),
         ]
 
+    else:
+        raise ValueError("Failed not open for {!r})".format(name))
+
+    path = None
     for p in paths:
         if p is not None:
             if os.path.exists(p):
@@ -29,13 +46,13 @@ def config(section='default'):
     if path is None:
         raise Exception("Config file default.conf couldn't be found")
 
-    if sys.version_info.major > 2 and sys.version_info.minor > 3:
-        config = ConfigParser.ConfigParser()
-    else:
+    if six.PY3:
         config = ConfigParser.SafeConfigParser()
+    else:
+        config = ConfigParser.ConfigParser()
 
     config.read([path])
-    configuration = dict(config.items(section))
+    configuration = dict(config.items(name))
 
     return configuration
 
