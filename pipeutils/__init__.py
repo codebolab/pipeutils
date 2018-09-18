9ff73fc469965180923f5b519a17332b41c84840
Elvis Ramirez <elvikito@gmail.com>
2018-09-18 14:41:54 -0400

    card-12732859 (#2)
    * configuration file.
    
    * test added.
    
    * Cambios que se necesitaban.
    
    * Otros cambios necesarios.

---
+++ pipeutils/__init__.py
@@ -0,0 +1,58 @@
+import os
+import six
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+if six.PY3:
+    import configparser as ConfigParser
+else:
+    import ConfigParser
+
+HOME = os.path.expanduser("~")
+CONFIG_PATH = os.environ.get('PIPE_CONFIG_PATH', os.path.join(HOME, '.pipeutils', 'config'))
+
+
+def config(name='base', dir_path=CONFIG_PATH):
+    '''
+    Args:
+        name (str): The `name` is name of config file.
+    Returns:
+        (dict) a configuration read from a file found in CONFIG_PATH
+    '''
+
+    path = None
+
+    # check file exists
+    _file = os.path.join(dir_path, '%s.conf' % name)
+    if os.path.exists(os.path.join(dir_path, _file)):
+        path = os.path.join(dir_path, _file)
+    else:
+        raise ConfigNotFound("Failed not open for {!r})".format(name), 'ConfigNotFound')
+
+    if six.PY3:
+        config = ConfigParser.ConfigParser()
+    else:
+        config = ConfigParser.SafeConfigParser()
+
+    config.read([path])
+    configuration = dict(config.items(name))
+
+    return configuration
+
+
+class ConfigNotFound(ValueError):
+
+    def __init__(self, arg):
+        self.strerror = arg
+        self.args = {arg}
+
+        try:
+            raise ConfigNotFound("Failed not open File not found.")
+        except ConfigNotFound as e:
+            print("ConfigNotFound Exception!", e.strerror)
+
+
+
+PIPE_PATH = os.path.os.path.dirname(__file__)
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+    'python-dateutil>=2.7.3',
+    'pytz>=2015.7',
+    'pyzmq>=17.1.2',
+    'Sphinx>=1.7.8',
+    'sphinx-rtd-theme>=0.4.1',
+    'sphinxcontrib-websupport>=1.1.0',
+    'tzlocal>=1.5.1'
+]
+
+setup(name='pipeutils',
+      version='0.1',
+      description='The pipeutils',
+      url='https://github.com/codebolab/pipeutils.git',
+      author='Code.bo',
+      author_email='info@code.bo',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=install_requires,
+)
