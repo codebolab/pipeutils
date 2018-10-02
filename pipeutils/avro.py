067969ad9e146a1c0c6a5b50597555b6ff1468b7
elvis ramirez <elvikito@gmail.com>
2018-10-02 12:02:31 -0400

    Changed.

--- pipeutils/avro.py
+++ pipeutils/avro.py
@@ -1,4 +1,5 @@
 import os
+import avro.schema
 
 HOME = os.path.expanduser("~")
 VERSION = 1
@@ -12,6 +13,8 @@ class Registry:
     '''
     def __init__(self, path=None):
         self.schema = {}
+        self.avro_schema = None
+        self.found_schema = False
         if path is not None:
             self.path = path
         elif os.environ.get('PIPE_SCHEMA_REGISTRY'):
@@ -28,14 +31,19 @@ class Registry:
         if os.path.exists(_schema):
             for f in os.listdir(_schema):
                 if f.endswith('.avsc') and int(os.path.splitext(f)[0]) == version:
+                    self.found_schema = True
+                    _file = os.path.join(self.path, name, f)
+                    self.avro_schema = avro.schema.Parse(open(_file, "rb").read())
                     with open(os.path.join(self.path, name, f)) as a:
                         key = '%s_%s' % (name, str(version))
                         self._cache(key, a.read())
+                elif not self.found_schema and int(os.path.splitext(f)[0]) < version:
+                    print('Other Version >  %s ' % int(os.path.splitext(f)[0]))
                 else:
                     raise SchemaVersionNotFound
         else:
             raise SchemaNotFound
-        return self.schema
+        return self.avro_schema, self.found_schema
 
 
 class SchemaNotFound(Exception):
--- tests/test_registry.py
+++ tests/test_registry.py
@@ -12,7 +12,7 @@ path = os.path.dirname(os.path.realpath(__file__))
 path_configs = os.path.join(path, 'configs')
 
 _schema = json.dumps({"type": "record", "name": "X", "fields": [{"name": "y", "type": {"type": "record", "name": "Y", "fields": [{"name": "Z", "type": "X"}]}}]})
-version = 1
+version = 2
 name = 'test'
 
 
@@ -26,18 +26,15 @@ class TestRegistry(unittest.TestCase):
         logger.info("testing")
 
         register = Registry(path_configs)
-        test = register.get(name, version)
-        key = '%s_%s' % (name, str(version))
+        r_schema, found_scheme = register.get(name, version)
+        logger.info(found_scheme)
 
-        logger.info(schema.Parse(test[key]))
         if six.PY3:
             original_parse = schema.Parse(_schema)
-            result_parse = schema.Parse(test[key])
         else:
             original_parse = schema.parse(_schema)
-            result_parse = schema.parse(test[key])
 
-        self.assertEqual(result_parse, original_parse)
+        self.assertEqual(r_schema, original_parse)
 
     def test_fail_schema_version(self):
         """
@@ -47,9 +44,24 @@ class TestRegistry(unittest.TestCase):
         logger.setLevel(logging.DEBUG)
         logger.info("testing")
         # not found version 2
-        version = 2
+        version = 1
         self.register = Registry(path_configs)
         self.assertRaises(SchemaVersionNotFound, lambda: self.register.get(name, version))
+
+    def test_fail_schema_version(self):
+        """
+           To test exception raise due to run time error
+        """
+
+        logger.setLevel(logging.DEBUG)
+        logger.info("testing")
+        # not found version 2
+        version = 3
+        self.register = Registry(path_configs)
+        _schema, found_scheme = self.register.get(name, version)
+
+        logger.info(found_scheme)
+        self.assertEquals(found_scheme, False)
 
 if __name__ == '__main__':
     unittest.main()
