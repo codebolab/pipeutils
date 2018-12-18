4408a92a59c2f13a82b668674516c6ada42afccc
elvis ramirez <elvikito@gmail.com>
2018-12-18 13:13:47 -0400

    Client for s3 and gdrive.

---
+++ pipeutils/clients/client_s3.py
@@ -0,0 +1,61 @@
+import os
+import boto3
+import botocore
+
+from pipeutils import logger
+from pipeutils import config
+
+HOME = os.path.expanduser("~")
+CONFIG_PATH = os.environ.get('PIPE_CONFIG_CLIENTS', os.path.join(HOME, '.pipeutils'))
+
+s3 = config('s3', CONFIG_PATH)
+
+
+class ClientS3(object):
+    session = boto3.Session(aws_access_key_id=s3['aws_access_key_id'],
+                            aws_secret_access_key=s3['aws_secret_access_key'])
+    clientS3 = session.resource('s3')
+
+    def __init__(self, bucket):
+        self.bucket = bucket
+
+    def upload(self, path, s3path):
+        '''
+        Args:
+            path (str): The `path` of file.
+            s3path (str): The s3path direction in bucket into s3
+        '''
+        try:
+            data = open(path, 'rb')
+            self.clientS3.Bucket(self.bucket).put_object(Key=s3path, Body=data)
+            data.close()
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                logger.error('The object does not exist.')
+            else:
+                raise
+
+    def download(self, s3path, path):
+        '''
+        Args:
+            s3path (str): The s3path direction in bucket into s3
+            path (str): The `path` of file where will it be download.
+        '''
+        try:
+            self.clientS3.Bucket(self.bucket).download_file(s3path, path)
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                logger.error('The object does not exist.')
+            else:
+                raise
+
+    def list(self):
+        """
+        List objects into bucket
+        """
+        objects = []
+        bucket = self.clientS3.Bucket(self.bucket)
+        for s3_file in bucket.objects.all():
+            logger.info('File > %s' % s3_file.key)
+            objects.append(s3_file.key)
+        return objects
---
+++ tests/test_clientGdrive.py
@@ -0,0 +1,47 @@
+import unittest
+import os
+import logging
+import json
+import gdrive_client
+
+from pipeutils import config
+from pipeutils.clients_GDrive import GDrive as DriveClient
+from pipeutils import logger
+PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+class TestCientGdrive(unittest.TestCase):
+    def get_client(self):
+        client = DriveClient.GDrive()
+        return client
+    '''
+    def test_exis_file(self):
+        client = self.get_client()
+        #result = client.search("name contains 'file_test_uload.txt' and mimeType != 'application/vnd.google-apps.folder'", sort='modifiedTime desc')
+        result = client.exists('file_test_upload.txt')
+        logger.info('result %s' % result)
+    ''' 
+
+    def test_get_list(self):
+        client = self.get_client()
+        files = client.listfiles()
+        assert len(files) > 0
+        assert files is not None
+        logger.info('Files %s' % files)
+
+    def test_download(self):
+        client = self.get_client()
+        path = os.path.join(os.getcwd(), 'files')
+        client.download('file1.txt', path)
+        assert os.path.exists(path+'/file1.txt') is True
+
+    def test_upload(self):
+        client = self.get_client()
+        cwd_dir = os.path.join(os.getcwd(), 'files')
+        file_upload = os.path.join(cwd_dir, "file1.txt")
+        upload = client.upload(file_upload, 'test')
+        assert upload is not None
+  
+if __name__ == '__main__':
+    unittest.main()
+
