15a11d1372cd6e066df4185a2c8cc3f05f09371b
diego zambrana <contacto@diegozambrana.com>
2019-03-26 12:18:10 -0400

    fix errors

--- pipeutils/utils.py
+++ pipeutils/utils.py
@@ -1,4 +1,5 @@
 import hashlib
+import six
 
 
 def generate_id(*args):
@@ -6,6 +7,7 @@ def generate_id(*args):
     Generate a code based in md5, receive a tuple of strings and return a 
     number string of 32 characters.
     """
+    concated = ''.join([six.text_type(x) for x in args])
     m = hashlib.md5()
-    m.update(''.join(args).encode('utf-8'))
-    return str(int(m.hexdigest(), 16))[0:32]
+    m.update(concated.encode('utf-8'))
+    return str(m.hexdigest())
