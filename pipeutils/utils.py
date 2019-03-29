fb38a97a3beca18e5a06ddd93f90d58ef755780d
diego zambrana <contacto@diegozambrana.com>
2019-03-29 12:56:53 -0400

    create generate_uuid

--- pipeutils/utils.py
+++ pipeutils/utils.py
@@ -1,5 +1,6 @@
 import hashlib
 import six
+import uuid
 
 
 def generate_id(*args):
@@ -11,3 +12,12 @@ def generate_id(*args):
     m = hashlib.md5()
     m.update(concated.encode('utf-8'))
     return six.text_type(m.hexdigest())
+
+
+def generate_uuid(phrase, size=8):
+    """
+    Generate a code base in the DNS namcespace, receive the params phrase that
+    is a string and a size that by default is integer 8 and return  a string
+    with the uuid code generated.
+    """
+    return str(uuid.uuid3(uuid.NAMESPACE_DNS, phrase))[:size]
