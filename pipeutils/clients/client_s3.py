a0613d2296bdcfcbd9386e5d09b0144809796499
diego zambrana <contacto@diegozambrana.com>
2019-04-01 11:22:08 -0400

    update upload_multiple

--- pipeutils/clients/client_s3.py
+++ pipeutils/clients/client_s3.py
@@ -35,12 +35,12 @@ class ClientS3(object):
     def upload_multiple(self, path, s3path, extension=None):
         '''
         Args:
-            path (str): the `path` of the file.
+            path (str): the directory path of the file.
             s3path (str): the s3path direction directory in the bucket into s3.
             extension (str): the extension of file to upload.
         '''
         for name_file in os.listdir(path):
-            if type_file is None or type_file in name_file:
+            if extension is None or extension in name_file:
                 source_path =  os.path.join(path, name_file)
                 s3_path = os.path.join(s3path, name_file)
                 self.upload(source_path, s3_path)
