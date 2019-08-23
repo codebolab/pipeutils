7e6033b40110c94d3304853e510aa37d285b98b2
elvis <elvikito@gmail.com>
2019-08-23 15:34:06 -0400

    fixed.

--- pipeutils/timestamps.py
+++ pipeutils/timestamps.py
@@ -33,15 +33,15 @@ def iso_utc_timezone(ts="America/New_York"):
     '''
     Return current local time in America/New_York and string representing
     '''
-    return ts_now_timezone(ts).strftime('%Y-%m-%d %H:%M:%S')
+    return ts_now_timezone(ts).isoformat()
+
 
 def today(ts="America/New_York"):
     """
     Returns a date object for today but using the timezone `ts`
     """
+    return ts_now_timezone(ts).date()
 
-    nyc = timezone(ts)
-    return datetime.today().astimezone(nyc)
 
 def iso_today(ts="America/New_York"):
     """
@@ -49,6 +49,7 @@ def iso_today(ts="America/New_York"):
     but using the timezone 'ts'
     """
     return today(ts).strftime(ISOFORMAT)
+
 
 def str_today(ts="America/New_York", format='%Y-%m-%d'):
     """
